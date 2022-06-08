7cdf4f409f5d7d1451265bb832839a6b836c95bc
09khu <khushbunemade90@gmail.com>
2022-06-08 21:34:31 +0530

    changes

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { ArrowDown } from "react-feather";
 import styles from "./body.module.css";
-
+//hiii repo
 function Body() {
     const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
     return (
